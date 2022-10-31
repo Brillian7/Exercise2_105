97ffff47902d34ec0e89b4cd2dd44678f1a6ec1c
Brilly <ASUS@LAPTOP-BEUE1DIV>
2022-10-31 16:02:43 +0700

    menambahkan while true untuk suatu iterasi  dengan memberikan nilai minimum dan maximum

--- Exercise2_105/Program.cs
+++ Exercise2_105/Program.cs
@@ -10,7 +10,23 @@ namespace Exercise2_105
     {
         //definisikan array dengan ukuran maksimum 45
         private int[] BA = new int[45];
+        //variable ukuran array
+        private int n;
+        //fungsi untuk menginputkan ukuran array
+        public void read()
+        {
+            while(true)
+            {
+                Console.WriteLine("Masukkan jumlah element array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 45))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimum 1 and maximum 45 elements.\n");
 
+            }
+        }
 
         static void Main(string[] args)
         {
