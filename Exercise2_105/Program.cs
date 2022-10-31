979f85b782bbb73bce219aeace14b269f9d416c8
Brilly <ASUS@LAPTOP-BEUE1DIV>
2022-10-31 15:42:37 +0700

    Add project files.

---
+++ Exercise2_105/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2_105
+{
+    class Program
+    {
+        //definisikan array dengan ukuran maksimum 45
+        private int[] BA = new int[45];
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
